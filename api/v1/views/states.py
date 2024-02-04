9c22fcd02aa69abcbd01a192e1bbe9904d3c127e
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-02-04 15:56:25 +0100

    Refactor state views and add error handling

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -7,21 +7,20 @@ from models.state import State
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
-@app_views.route('/states/', methods=['GET'], strict_slashes=False)
 def list_states():
     '''GET all State'''
-    states = [obj.to_dict() for obj in storage.all("State").values()]
-    return jsonify(states)
+    states = storage.all("State").values()
+    list_states = [state.to_dict() for state in states]
+    return jsonify(list_states)
 
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_state(state_id):
     '''GET State '''
-    all_states = storage.all("State").values()
-    states = [obj.to_dict() for obj in all_states if obj.id == state_id]
-    if states == []:
-        abort(404)
-    return jsonify(states[0])
+    state = storage.get(State, state_id)
+    if not state:
+        return make_response(jsonify({"error": "Not found"}))
+    return jsonify(state.to_dict())
 
 
 @app_views.route('/states/<state_id>', methods=['DELETE'],
@@ -40,34 +39,31 @@ def delete_state(state_id):
     return jsonify({}), 200
 
 
-@app_views.route('/states/', methods=['POST'], strict_slashes=False)
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
     '''POST a State'''
     if not request.get_json():
-        return make_response(jsonify({'error': 'Not a JSON'}), 400)
+        return abort("Not a JSON", 400)
     if 'name' not in request.get_json():
         return make_response(jsonify({'error': 'Missing name'}), 400)
-    states = []
-    new_state = State(name=request.json['name'])
-    storage.new(new_state)
-    storage.save()
-    states.append(new_state.to_dict())
-    return jsonify(states[0]), 201
+    data = request.get_json()
+    new_state = State(**data)
+    new_state.save()
+    return jsonify(new_state.to_dict())
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def updates_state(state_id):
     '''Updates a State object'''
-    all_states = storage.all("State").values()
-    states = [obj.to_dict() for obj in all_states if obj.id == state_id]
-    if states == []:
+    state = storage.get(State, state_id)
+    if state is None:
         abort(404)
-    if not request.get_json():
-        return make_response(jsonify({'error': 'Not a JSON'}), 400)
-    if 'name' in request.get_json():
-        states[0]['name'] = request.json['name']
-    for obj in all_states:
-        if obj.id == state_id:
-            obj.name = request.json['name']
-    storage.save()
-    return jsonify(states[0]), 200
+    data = request.get_json()
+    if data is None:
+        abort("Not a JSON", 404)
+    attributes = ['created_at', 'updated_at', 'id']
+    for key, val in data.items():
+        if key not in attributes:
+            setattr(state, key, val)
+    state.save()
+    return jsonify(state.to_dict()), 200
