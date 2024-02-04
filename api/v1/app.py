36d1e43656e071161dd48d9537675536596252e9
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-02-04 15:31:53 +0100

    Add CORS support to Flask app

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,9 +8,11 @@ from api.v1.views import app_views
 import os
 from models import storage
 from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 
 app = Flask('__name__')
 app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 if os.getenv("HBNB_API_HOST"):
     HBNB_HOST = os.getenv("HBNB_API_HOST")
