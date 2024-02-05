a7d05f35db8776df43bf65b5198adb832f73912d
abdelhadia72 <thisisavnb@gmail.com>
2024-02-05 19:48:06 +0100

    updaing docments 2.0

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""
-App views for AirBnB_clone_v3
+"""App views for AirBnB_clone_v3
+
+Indexing app views
 """
 
 from flask import jsonify
@@ -10,14 +11,24 @@ from api.v1.views import app_views
 
 @app_views.route('/status')
 def status():
-    """ returns status """
+    """
+    Returns the status of the API.
+
+    :return: A JSON object containing the status.
+    """
     status = {"status": "OK"}
     return jsonify(status)
 
 
 @app_views.route('/stats')
 def count():
-    """ returns number of each objects by type """
+    """
+    Returns the number of each object by type.
+
+    Returns:
+        A JSON object containing the count of each object type.
+
+    """
     alls = {}
     classes = {"Amenity": "amenities",
                "City": "cities",
@@ -29,3 +40,5 @@ def count():
         count = storage.count(classx)
         alls[classes.get(classx)] = count
     return jsonify(alls)
+
+
