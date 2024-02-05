28690761b8cabfc64a39eb6d3a273a2458ef720e
abdelhadia72 <thisisavnb@gmail.com>
2024-02-05 11:13:16 +0100

    Fixing get method and chagne it

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -71,7 +71,7 @@ class FileStorage:
 
     def get(self, cls, id):
         """retrieve an object from the database by class and id"""
-        return self.all(cls).get(f"{cls.__name__}.{id}")
+        return self.all(cls).get(cls.__name__ + '.' + id)
 
     def count(self, cls=None):
         """count the number of objects in the database"""
