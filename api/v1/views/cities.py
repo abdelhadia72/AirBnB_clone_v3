ecbc51642c80ec3ebf57e32b9974d361572ba214
abdelhadia72 <thisisavnb@gmail.com>
2024-02-05 19:42:52 +0100

    update doc

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -10,7 +10,7 @@ from models.state import State
 @app_views.route('/states/<state_id>/cities', methods=['GET'],
                  strict_slashes=False)
 def list_cities(state_id):
-    '''GET all City objects'''
+    """ GET all City objects """
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
@@ -20,7 +20,7 @@ def list_cities(state_id):
 
 @app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
 def get_city(city_id):
-    '''GET City object'''
+    """ GET City object """
     city = storage.get(City, city_id)
     if city is None:
         abort(404)
@@ -30,7 +30,7 @@ def get_city(city_id):
 @app_views.route('/cities/<city_id>', methods=['DELETE'],
                  strict_slashes=False)
 def delete_city(city_id):
-    '''DELETE City object'''
+    """ DELETE City object """
     city = storage.get(City, city_id)
     if city is None:
         abort(404)
@@ -42,7 +42,7 @@ def delete_city(city_id):
 @app_views.route('/states/<state_id>/cities', methods=['POST'],
                  strict_slashes=False)
 def create_city(state_id):
-    '''POST a City object'''
+    """ POST a City object """
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
