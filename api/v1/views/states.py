4ebbce67910239ec3a2a71ecca0573ca7e8dedbe
abdelhadia72 <thisisavnb@gmail.com>
2024-02-05 17:50:25 +0100

    change how to delete in the DELETE phrse

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -31,7 +31,7 @@ def delete_state(state_id):
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
-    storage.delete(state)
+    state.delete()
     storage.save()
     return jsonify({}), 200
 
